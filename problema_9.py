b47b59e0f5a6fc5fe7f1c74bcf7f188608b85bd4
JIMMY1905 <134471772+JIMMY1905@users.noreply.github.com>
2024-01-26 05:01:05 +0000

    subiendo_9

---
+++ problema_9.py
@@ -0,0 +1,7 @@
+
+lista_original = ['Di', 'buen', 'día', 'a', 'papa']
+lista_invertida = list(reversed(lista_original))
+print("Lista original:", lista_original)
+print("Lista invertida:", lista_invertida)
+
+
